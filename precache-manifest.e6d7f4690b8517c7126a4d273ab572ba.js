86c8251d5415564033de90ce007ebfc0dfd982c3
Vladislav Vaganov <VladislavVaganov@mysale.com>
2020-07-21 14:55:35 +0300

    Updates

---
+++ precache-manifest.e6d7f4690b8517c7126a4d273ab572ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e7579b259d5d86dd4a1ce73ad3382c",
+    "url": "/simple-loan-calculator/index.html"
+  },
+  {
+    "revision": "ab37459915ff7d05837a",
+    "url": "/simple-loan-calculator/static/css/main.6c7ca0c4.chunk.css"
+  },
+  {
+    "revision": "072df1a952d396d5ca8d",
+    "url": "/simple-loan-calculator/static/js/2.b7e9d6e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-loan-calculator/static/js/2.b7e9d6e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab37459915ff7d05837a",
+    "url": "/simple-loan-calculator/static/js/main.306218b2.chunk.js"
+  },
+  {
+    "revision": "351f8e34094f94adb3f7",
+    "url": "/simple-loan-calculator/static/js/runtime-main.d6149095.js"
+  }
+]);
